98047d3a20e410426d1713a08fc60fe17bd1b018
Ruymán Borges R <ruyman21@gmail.com>
2022-05-21 23:23:12 +0100

    Adding main.go clean to repo with brief description

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+ * File:    main.go
+ *
+ * Author:  	Ruymán Borges R. (ruyman21@gmail.com)
+ * Date:    	21-5-22
+ *
+ * Summary of File:
+ *
+ *  This program runs a service that exposes an graphQL API which allows people to create, update, delete and retrieve answers as key-value pairs.
+ * 	The answers are stored in a persistent storage medium so they can handle service restarts.
+ *  
+ *
+ */
