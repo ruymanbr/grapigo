7fba3c93a61034ffdcbc17099250b5442ccdd0d1
Ruymán Borges R <ruyman21@gmail.com>
2022-05-22 15:37:46 +0100

    Added basic structure of main.go. Correction

--- main.go
+++ main.go
@@ -13,36 +13,6 @@
  */
 
 
-/*
- * If a user saves the same key multiple times (using update), every answer should be saved. When retrieving an answer, it should return the latest answer.
- *
- * If a user tries to create an answer that already exists - the request should fail and an adequate message or code should be returned.
- *
- * If an answer doesn't exist or has been deleted, an adequate message or code should be returned.
- *
- * When returning history, only mutating events (create, update, delete) should be returned. The "get" events should not be recorded.
- *
- * It is possible to create a key after it has been deleted. However, it is not possible to update a deleted key. For example the following event sequences are allowed:
- *
- * create → delete → create → update
- *
- * create → update → delete → create → update
- *
- * However, the following should not be allowed:
- *
- * create → delete → update
- *
- * create → create
- * 
- * Additional questions:
- *
- * How would you support multiple users?
- * How would you support answers with types other than string
- * What are the main bottlenecks of your solution?
- * How would you scale the service to cope with thousands of requests?
- *
- */
-
 package main
 
 import (
